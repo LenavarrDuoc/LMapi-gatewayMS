8a3f4ad5c1b31296505f09446cc29f08013039f3
LenavarrDuoc <leo.navarr@duocuc.cl>
2026-05-11 02:33:22 -0400

    Hasta ahora, configurados los microservicios de Customer, Store, y Order

--- src/main/java/cl/duoc/apigateway/ApiGatewayApplication.java
+++ src/main/java/cl/duoc/apigateway/ApiGatewayApplication.java
@@ -2,7 +2,9 @@ package cl.duoc.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient //Viene incluida por defecto la anotación en Spring Boot moderno, pero se recomienda explicitarla para tener claro lo que estamos utilizando.
 @SpringBootApplication
 public class ApiGatewayApplication {
 
